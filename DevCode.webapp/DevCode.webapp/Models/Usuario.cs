b746c1885d87dd8d8fbb63701aa49c5691880fb6
Guilherme Marques <guilherme.marques.santos457@gmail.com>
2023-03-03 21:28:34 -0300

    add-aspnet-project

---
+++ DevCode.webapp/DevCode.webapp/Models/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DevCode.webapp.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        public string Email { get; set; }
+
+        [DisplayName("Senha")]
+        [Required(ErrorMessage = "Campo {0} é obrigatório")]
+        public string Senha { get; set; }
+
+       
+    }
+}
